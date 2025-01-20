09c49649e773768c8001b74771f026d25019bf6e
Muzahidul-Islam-Zehad <zehadkazi88@gmail.com>
2025-01-20 11:49:18 +0600

    pagination added on admin dashboard

--- src/Pages/Dashboard/AllArticles/AllArticlesAdmin.jsx
+++ src/Pages/Dashboard/AllArticles/AllArticlesAdmin.jsx
@@ -6,12 +6,15 @@ import { format } from "date-fns";
 import { useState } from "react";
 import Swal from "sweetalert2";
 import DeclineModal from "./DeclineModal/DeclineModal";
+import ReactPaginate from "react-paginate";
 
 const AllArticlesAdmin = () => {
     const axiosSecure = useAxiosSecure();
     const [loading, setLoading] = useState();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [decliningId, setDecliningId] = useState('');
+    const [currentPage, setCurrentPage] = useState(0); // Current page (1-based)
+    const [limit, setLimit] = useState(3); // Items per pageF
 
     const handleOpenModal = () => setIsModalOpen(true);
     const handleCloseModal = () => {
@@ -20,16 +23,20 @@ const AllArticlesAdmin = () => {
     }
 
     const { data: allArticle = [], isLoading, refetch } = useQuery({
-        queryKey: ['all-articles'],
+        queryKey: ['all-articles', currentPage, limit],
         queryFn: async () => {
-            const { data } = await axiosSecure.get('/all-articles/data')
+            const { data } = await axiosSecure.get(`/all-articles/data?limit=${limit}&page=${currentPage + 1}`)
             return data;
         }
     })
 
-    if (isLoading || loading) {
-        return <LoadingSpinner></LoadingSpinner>
-    }
+    const handlePageClick = (event) => {
+        setCurrentPage(event.selected); // Update the current page (0-based)
+    };
+
+    // if (isLoading || loading) {
+    //     return <LoadingSpinner></LoadingSpinner>
+    // }
 
     const handleAproveArticle = async (id) => {
         setLoading(true);
@@ -158,89 +165,128 @@ const AllArticlesAdmin = () => {
             </div>
             <div>
                 <div className="overflow-x-auto w-full p-6">
-                    <table className="table w-full border border-gray-300 rounded-lg shadow-md">
-                        {/* Table Head */}
-                        <thead className="bg-gray-200">
-                            <tr>
-                                <th className="py-3 px-4 text-left">#</th>
-                                <th className="py-3 px-4 text-left">Article Title</th>
-                                <th className="py-3 px-4 text-left">Author</th>
-                                <th className="py-3 px-4 text-left">Email</th>
-                                <th className="py-3 px-4 text-left">Posted Date</th>
-                                <th className="py-3 px-4 text-left">Status</th>
-                                <th className="py-3 px-4 text-left">Publisher</th>
-                                <th className="py-3 px-4 text-left">Approve</th>
-                                <th className="py-3 px-4 text-left">Decline</th>
-                                <th className="py-3 px-4 text-left">Delete</th>
-                                <th className="py-3 px-4 text-left">Make Premium</th>
-                            </tr>
-                        </thead>
-                        {/* Table Body */}
-                        <tbody>
-                            {/* table Row */}
-                            {
-                                allArticle.map((article, idx) => <tr key={article?._id} className="hover:bg-gray-100 border-t">
-                                    <td className="py-3 px-4">{idx + 1}</td>
-                                    <td className="py-3 px-4">{
-                                        article.articleTitle.length > 40 ? article.articleTitle.substring(0,30)+'...' : article.articleTitle
-                                        }</td>
-                                    <td className="py-3 px-4">
-                                        <div className="flex items-center gap-4">
-                                            <div className="w-12 h-12 rounded-full overflow-hidden">
-                                                <img
-                                                    src={article?.userInfo?.image}
-                                                    alt="Author"
-                                                    className="object-cover w-full h-full"
-                                                />
-                                            </div>
-                                            <span>{article?.userInfo?.name}</span>
-                                        </div>
-                                    </td>
-                                    <td className="py-3 px-4">{article?.userInfo?.email}</td>
-                                    <td className="py-3 px-4">{format(new Date(article?.createdAt), 'PPPP')}</td>
-                                    <td className="py-3 px-4">
-                                        <span className="px-3 py-1 text-white rounded-md bg-yellow-500">
-                                            {article?.status}
-                                        </span>
-                                    </td>
-                                    <td className="py-3 px-4">{article.publisher}</td>
-                                    <td className="py-3 px-4">
-                                        <button disabled={article.status === 'Approved' || article.status === 'Declined'} onClick={() => handleAproveArticle(article._id)} className="btn btn-md bg-green-500 text-white hover:bg-green-600 rounded-md px-3 py-1">
-                                            Approve
-                                        </button>
-                                    </td>
-                                    <td className="py-3 px-4">
-                                        <button onClick={() => { handleOpenModal(), setDecliningId(article._id) }} disabled={article.status === 'Approved' || article.status === 'Declined'}
-                                            className="btn btn-md bg-red-500 text-white hover:bg-red-600 rounded-md px-3 py-1">
-                                            Decline
-                                        </button>
-                                    </td>
-                                    <td className="py-3 px-4">
-                                        <button onClick={() => handleDelete(article._id)} className="btn btn-md bg-blue-500 text-white hover:bg-blue-600 rounded-md px-3 py-1">
-                                            Delete
-                                        </button>
-                                    </td>
-                                    <td className="py-3 px-4">
-                                        {
-                                            article.isPremium === 'Yes'
-                                                ?
-                                                <span>Premium</span>
-                                                :
+                    {
+                        isLoading || loading ? <LoadingSpinner></LoadingSpinner>
+                            :
+                            <table className="table w-full border border-gray-300 rounded-lg shadow-md">
+                                {/* Table Head */}
+                                <thead className="bg-gray-200">
+                                    <tr>
+                                        <th className="py-3 px-4 text-left">#</th>
+                                        <th className="py-3 px-4 text-left">Article Title</th>
+                                        <th className="py-3 px-4 text-left">Author</th>
+                                        <th className="py-3 px-4 text-left">Email</th>
+                                        <th className="py-3 px-4 text-left">Posted Date</th>
+                                        <th className="py-3 px-4 text-left">Status</th>
+                                        <th className="py-3 px-4 text-left">Publisher</th>
+                                        <th className="py-3 px-4 text-left">Approve</th>
+                                        <th className="py-3 px-4 text-left">Decline</th>
+                                        <th className="py-3 px-4 text-left">Delete</th>
+                                        <th className="py-3 px-4 text-left">Make Premium</th>
+                                    </tr>
+                                </thead>
+                                {/* Table Body */}
+                                <tbody>
+                                    {/* table Row */}
 
-                                                <button disabled={article.status === 'Declined'} onClick={() => handleMakePremium(article._id)} className="btn btn-md bg-purple-500 text-white hover:bg-purple-600 rounded-md px-3 py-1">
-                                                    Make Premium
+                                    {
+                                        allArticle?.articles.map((article, idx) => <tr key={article?._id} className="hover:bg-gray-100 border-t">
+                                            <td className="py-3 px-4">{idx + 1}</td>
+                                            <td className="py-3 px-4">{
+                                                article.articleTitle.length > 40 ? article.articleTitle.substring(0, 30) + '...' : article.articleTitle
+                                            }</td>
+                                            <td className="py-3 px-4">
+                                                <div className="flex items-center gap-4">
+                                                    <div className="w-12 h-12 rounded-full overflow-hidden">
+                                                        <img
+                                                            src={article?.userInfo?.image}
+                                                            alt="Author"
+                                                            className="object-cover w-full h-full"
+                                                        />
+                                                    </div>
+                                                    <span>{article?.userInfo?.name}</span>
+                                                </div>
+                                            </td>
+                                            <td className="py-3 px-4">{article?.userInfo?.email}</td>
+                                            <td className="py-3 px-4">{format(new Date(article?.createdAt), 'PPPP')}</td>
+                                            <td className="py-3 px-4">
+                                                <span className="px-3 py-1 text-white rounded-md bg-yellow-500">
+                                                    {article?.status}
+                                                </span>
+                                            </td>
+                                            <td className="py-3 px-4">{article.publisher}</td>
+                                            <td className="py-3 px-4">
+                                                <button disabled={article.status === 'Approved' || article.status === 'Declined'} onClick={() => handleAproveArticle(article._id)} className="btn btn-md bg-green-500 text-white hover:bg-green-600 rounded-md px-3 py-1">
+                                                    Approve
                                                 </button>
-                                        }
-                                    </td>
+                                            </td>
+                                            <td className="py-3 px-4">
+                                                <button onClick={() => { handleOpenModal(), setDecliningId(article._id) }} disabled={article.status === 'Approved' || article.status === 'Declined'}
+                                                    className="btn btn-md bg-red-500 text-white hover:bg-red-600 rounded-md px-3 py-1">
+                                                    Decline
+                                                </button>
+                                            </td>
+                                            <td className="py-3 px-4">
+                                                <button onClick={() => handleDelete(article._id)} className="btn btn-md bg-blue-500 text-white hover:bg-blue-600 rounded-md px-3 py-1">
+                                                    Delete
+                                                </button>
+                                            </td>
+                                            <td className="py-3 px-4">
+                                                {
+                                                    article.isPremium === 'Yes'
+                                                        ?
+                                                        <span>Premium</span>
+                                                        :
 
-                                </tr>
-                                )
-                            }
+                                                        <button disabled={article.status === 'Declined'} onClick={() => handleMakePremium(article._id)} className="btn btn-md bg-purple-500 text-white hover:bg-purple-600 rounded-md px-3 py-1">
+                                                            Make Premium
+                                                        </button>
+                                                }
+                                            </td>
 
-                        </tbody>
-                    </table>
+                                        </tr>
+                                        )
+                                    }
+
+                                </tbody>
+                            </table>
+                    }
                 </div>
-
+            </div>
+            <div>
+                <div className="flex flex-col items-center gap-4">
+                    <ReactPaginate
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        breakLabel={"..."}
+                        pageCount={allArticle.totalPages}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={3}
+                        onPageChange={handlePageClick} // Handle page click
+                        containerClassName="flex items-center justify-center space-x-2 mt-4" // Overall pagination container
+                        pageClassName="px-3 py-1 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition" // Page item
+                        pageLinkClassName="text-gray-700" // Page link
+                        activeClassName="bg-blue-500 text-white" // Active page
+                        activeLinkClassName="font-bold text-white" // Active page link
+                        previousClassName="px-3 py-1 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition" // Previous button
+                        previousLinkClassName="text-gray-700" // Previous link
+                        nextClassName="px-3 py-1 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition" // Next button
+                        nextLinkClassName="text-gray-700" // Next link
+                        breakClassName="px-3 py-1" // Ellipsis button
+                        breakLinkClassName="text-gray-700" // Ellipsis link
+                        disabledClassName="opacity-50 cursor-not-allowed" // Disabled button
+                    />
+                    <div className="flex  gap-4">
+                        <p>Select Limit : </p>
+                        <select value={limit} onChange={((e) => setLimit(e.target.value))} className="select select-bordered w-full max-w-xs">
+                            <option value={3}>3</option>
+                            <option value={5}>5</option>
+                            <option value={10}>10</option>
+                            <option value={20}>20</option>
+                            <option value={50}>50</option>
+                        </select>
+                    </div>
+                </div>
             </div>
             <DeclineModal
                 isOpen={isModalOpen}
--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -3,18 +3,28 @@ import PageHeading from "../../../Components/SharedComponents/PageHeading";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import LoadingSpinner from "../../../Components/LoadingSpinner/LoadingSpinner";
 import Swal from "sweetalert2";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
 
 const AllUsers = () => {
 
     const axiosSecure = useAxiosSecure();
+    const [currentPage, setCurrentPage] = useState(0); // Current page (1-based)
+    const [limit, setLimit] = useState(3); // Items per pageF
 
     const { data: allUsers = [], isLoading, refetch } = useQuery({
-        queryKey: ['all-users'],
+        queryKey: ['all-users', currentPage, limit],
         queryFn: async () => {
-            const { data } = await axiosSecure.get('/all-users');
+            const { data } = await axiosSecure.get(`/all-users?page=${currentPage + 1}&limit=${limit}`);
             return data;
         }
     });
+
+    const handlePageClick = (event) => {
+        setCurrentPage(event.selected); // Update the current page (0-based)
+    };
+
+
 
     const handleUpdateToAdmin = async (id) => {
 
@@ -52,9 +62,11 @@ const AllUsers = () => {
         });
     }
 
-    if (isLoading) {
-        return <LoadingSpinner></LoadingSpinner>
-    }
+    // if (isLoading) {
+    //     return <LoadingSpinner></LoadingSpinner>
+    // }
+
+    console.log(allUsers.users);
 
     return (
         <div>
@@ -62,58 +74,100 @@ const AllUsers = () => {
                 <PageHeading title='All users' subtitle='All users of this application'></PageHeading>
             </div>
             <div>
-                <div className="overflow-x-auto w-full p-6">
-                    <table className="table w-full border border-gray-300 rounded-lg shadow-md">
-                        {/* Table Head */}
-                        <thead className="bg-gray-200">
-                            <tr>
-                                <th className="py-3 px-4 text-left">#</th>
-                                <th className="py-3 px-4 text-left">Profile</th>
-                                <th className="py-3 px-4 text-left">Name</th>
-                                <th className="py-3 px-4 text-left">Email</th>
-                                <th className="py-3 px-4 text-left">Action</th>
-                            </tr>
-                        </thead>
-                        {/* Table Body */}
-                        <tbody>
-                            {/* Row */}
-                            {
-                                allUsers.map((user, idx) =>
-                                    <tr key={user._id} className="hover:bg-gray-100 border-t">
-                                        <td className="py-3 px-4">{idx + 1}</td>
-                                        <td className="py-3 px-4">
-                                            <div className="flex items-center gap-3">
-                                                <div className="w-12 h-12 rounded-full overflow-hidden">
-                                                    <img
-                                                        src={user.photoURL}
-                                                        alt="Profile"
-                                                        className="object-cover w-full h-full"
-                                                    />
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td className="py-3 px-4">{user.name}</td>
-                                        <td className="py-3 px-4">{user.email}</td>
-                                        <td className="py-3 px-4 ">
-                                            {
-                                                user?.role === 'Admin'
-                                                    ?
-                                                    <span className="font-bold  bg-lime-200 py-1 px-3 rounded-xl">Admin</span>
-                                                    :
-                                                    <button onClick={() => handleUpdateToAdmin(user._id)} className="btn btn-md bg-blue-500 text-white hover:bg-blue-600 rounded-md px-4 py-2">
-                                                        Make Admin
-                                                    </button>
-                                            }
-
-                                        </td>
+                <div className="overflow-x-auto w-full p-6 min-h-[400px]">
+                    {
+                        isLoading
+                            ?
+                            <LoadingSpinner></LoadingSpinner>
+                            :
+                            <table className="table w-full border border-gray-300 rounded-lg shadow-md">
+                                {/* Table Head */}
+                                <thead className="bg-gray-200">
+                                    <tr>
+                                        <th className="py-3 px-4 text-left">#</th>
+                                        <th className="py-3 px-4 text-left">Profile</th>
+                                        <th className="py-3 px-4 text-left">Name</th>
+                                        <th className="py-3 px-4 text-left">Email</th>
+                                        <th className="py-3 px-4 text-left">Action</th>
                                     </tr>
-                                )
-                            }
+                                </thead>
+                                {/* Table Body */}
+                                <tbody>
+                                    {/* Row */}
+                                    {
+                                        allUsers?.users.map((user, idx) =>
 
-                        </tbody>
-                    </table>
+                                            <tr key={user._id} className="hover:bg-gray-100 border-t">
+                                                <td className="py-3 px-4">{idx + 1}</td>
+                                                <td className="py-3 px-4">
+                                                    <div className="flex items-center gap-3">
+                                                        <div className="w-12 h-12 rounded-full overflow-hidden">
+                                                            <img
+                                                                referrerPolicy="no-referrer"
+                                                                src={user.photoURL}
+                                                                alt="Profile"
+                                                                className="object-cover w-full h-full"
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td className="py-3 px-4">{user.name}</td>
+                                                <td className="py-3 px-4">{user.email}</td>
+                                                <td className="py-3 px-4 ">
+                                                    {
+
+                                                        user?.role === 'Admin'
+                                                            ?
+                                                            <span className="font-bold  bg-lime-200 py-1 px-3 rounded-xl">Admin</span>
+                                                            :
+                                                            <button onClick={() => handleUpdateToAdmin(user._id)} className="btn btn-md bg-blue-500 text-white hover:bg-blue-600 rounded-md px-4 py-2">
+                                                                Make Admin
+                                                            </button>
+                                                    }
+
+                                                </td>
+                                            </tr>
+                                        )
+                                    }
+
+                                </tbody>
+                            </table>
+                    }
                 </div>
 
+                <div className="flex flex-col items-center gap-4">
+                    <ReactPaginate
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        breakLabel={"..."}
+                        pageCount={allUsers.totalPages}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={3}
+                        onPageChange={handlePageClick} // Handle page click
+                        containerClassName="flex items-center justify-center space-x-2 mt-4" // Overall pagination container
+                        pageClassName="px-3 py-1 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition" // Page item
+                        pageLinkClassName="text-gray-700" // Page link
+                        activeClassName="bg-blue-500 text-white" // Active page
+                        activeLinkClassName="font-bold text-white" // Active page link
+                        previousClassName="px-3 py-1 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition" // Previous button
+                        previousLinkClassName="text-gray-700" // Previous link
+                        nextClassName="px-3 py-1 border border-gray-300 rounded hover:bg-blue-500 hover:text-white transition" // Next button
+                        nextLinkClassName="text-gray-700" // Next link
+                        breakClassName="px-3 py-1" // Ellipsis button
+                        breakLinkClassName="text-gray-700" // Ellipsis link
+                        disabledClassName="opacity-50 cursor-not-allowed" // Disabled button
+                    />
+                    <div className="flex  gap-4">
+                        <p>Select Limit : </p>
+                        <select value={limit} onChange={((e) => setLimit(e.target.value))} className="select select-bordered w-full max-w-xs">
+                            <option value={3}>3</option>
+                            <option value={5}>5</option>
+                            <option value={10}>10</option>
+                            <option value={20}>20</option>
+                            <option value={50}>50</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </div>
     );
