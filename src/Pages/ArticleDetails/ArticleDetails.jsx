9fae47f9b6cc3aad6ea1f1a6254adb8f0d7c2fcd
Muzahidul-Islam-Zehad <zehadkazi88@gmail.com>
2025-02-19 00:01:42 +0600

    article details page designed for dark mode

--- src/Pages/ArticleDetails/ArticleDetails.jsx
+++ src/Pages/ArticleDetails/ArticleDetails.jsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import PageHeading from "../../Components/SharedComponents/PageHeading";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner";
@@ -38,21 +37,21 @@ const ArticleDetails = () => {
     }
 
     return (
-        <div className="w-11/12 md:w-5/6 lg:w-3/5 mx-auto my-6">
-            <div>
+        <div className="w-11/12 md:w-5/6 lg:w-3/5 mx-auto py-6">
+            {/* <div>
                 <PageHeading />
-            </div>
-            <div className="p-6 border border-gray-300 rounded-3xl shadow-lg bg-white">
+            </div> */}
+            <div className="p-6 border border-gray-300 rounded-3xl shadow-lg bg-white dark:bg-gray-800 dark:border-gray-700">
                 <img
                     className="w-full h-[500px] object-cover bg-slate-100 rounded-xl shadow-md"
                     src={data?.articleImage}
                     alt="article-image"
                 />
-                <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold mt-4 text-gray-800">{data?.articleTitle}</h1>
+                <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold mt-4 text-gray-800 dark:text-gray-200">{data?.articleTitle}</h1>
                 <h3 className="mt-4 text-lg sm:text-xl font-medium text-white bg-gradient-to-r from-green-400 to-blue-500 px-4 py-2 rounded-full w-max">
                     Publisher: {data?.publisher}
                 </h3>
-                <p className="mt-6 text-lg sm:text-xl text-gray-600 leading-relaxed">{data?.articleDescription}</p>
+                <p className="mt-6 text-lg sm:text-xl text-gray-600 leading-relaxed dark:text-gray-400">{data?.articleDescription}</p>
             </div>
         </div>
 
