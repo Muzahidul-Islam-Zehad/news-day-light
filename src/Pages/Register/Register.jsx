6afffb26f9cad13471896cd8c121e675e700a0bd
Muzahidul-Islam-Zehad <zehadkazi88@gmail.com>
2025-02-19 08:03:29 +0600

    register page designed for darkmode

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -148,37 +148,37 @@ const Register = () => {
     }
 
     return (
-        <div className="flex justify-center items-center min-h-screen bg-[#F4F6F8] py-10">
-            <div className="card bg-base-100 w-full max-w-md shrink-0 shadow-2xl">
+        <div className="flex justify-center items-center min-h-screen bg-[#F4F6F8] py-10 dark:bg-gray-900">
+            <div className="card bg-base-100 w-full max-w-md shrink-0 shadow-2xl dark:bg-gray-800">
                 <div className="text-center pt-4">
                     <Link to={`/`} className="flex justify-center w-1/4 mx-auto"><img className="w-14" src={`https://i.ibb.co.com/Y7VfxdWN/news-Day-Light-Logo-Final.png`} alt="" /></Link>
-                    <h1 className="text-3xl font-bold text-[#003366]">Register Here</h1>
-                    <p className="text-[#6C757D]">Register to explore news</p>
+                    <h1 className="text-3xl font-bold text-[#003366] dark:text-gray-200">Register Here</h1>
+                    <p className="text-[#6C757D] dark:text-gray-400">Register to explore news</p>
                     <hr className="mt-3 w-5/6 mx-auto" />
                 </div>
                 <form onSubmit={handleSubmitForm} className="card-body pb-3">
                     {/* name field */}
                     <div className="form-control">
                         <label className="label ">
-                            <span className="label-text text-[#6C757D]">Name<span className="text-red-600 font-bold">*</span></span>
+                            <span className="label-text text-[#6C757D] dark:text-gray-300">Name<span className="text-red-600 font-bold">*</span></span>
                         </label>
-                        <input type="text" name="name" placeholder="name" className="input input-bordered bg-[#F9FAFB] border border-[#D1D5DB] text-[#333333] focus:ring-2 focus:ring-[#00B4D8] focus:outline-none focus:border-[#00B4D8] placeholder:text-[#A0AEC0]" />
+                        <input type="text" name="name" placeholder="name" className="input input-bordered bg-[#F9FAFB] border border-[#D1D5DB] text-[#333333] focus:ring-2 focus:ring-[#00B4D8] focus:outline-none focus:border-[#00B4D8] placeholder:text-[#A0AEC0] dark:bg-gray-700 dark:text-white" />
                     </div>
 
                     {/* email field */}
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text text-[#6C757D]">Email<span className="text-red-600 font-bold">*</span></span>
+                            <span className="label-text text-[#6C757D] dark:text-gray-300">Email<span className="text-red-600 font-bold">*</span></span>
                         </label>
-                        <input type="email" name="email" placeholder="email" className="input input-bordered bg-[#F9FAFB] border border-[#D1D5DB] text-[#333333] focus:ring-2 focus:ring-[#00B4D8] focus:outline-none focus:border-[#00B4D8] placeholder:text-[#A0AEC0]" />
+                        <input type="email" name="email" placeholder="email" className="input input-bordered bg-[#F9FAFB] border border-[#D1D5DB] text-[#333333] focus:ring-2 focus:ring-[#00B4D8] focus:outline-none focus:border-[#00B4D8] placeholder:text-[#A0AEC0] dark:bg-gray-700 dark:text-white" />
                     </div>
 
                     {/* Photo field */}
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text text-[#6C757D]">Photo<span className="text-red-600 font-bold">*</span></span>
+                            <span className="label-text text-[#6C757D] dark:text-gray-300">Photo<span className="text-red-600 font-bold">*</span></span>
                         </label>
-                        <label className="text-center border border-dashed hover:cursor-pointer border-gray-400 p-3 rounded-md bg-[#F9FAFB]">
+                        <label className="text-center border border-dashed hover:cursor-pointer border-gray-400 p-3 rounded-md bg-[#F9FAFB] dark:bg-gray-700 dark:text-white">
                             {
                                 photoName ? <span>{photoName}</span> : <span>Choose A Photo</span>
                             }
@@ -190,11 +190,11 @@ const Register = () => {
                     {/* password field */}
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text text-[#6C757D]">Password<span className="text-red-600 font-bold">*</span></span>
+                            <span className="label-text text-[#6C757D] dark:text-gray-300">Password<span className="text-red-600 font-bold">*</span></span>
                         </label>
                         <div className="relative">
-                            <input type={isVisible ? 'text' : 'password'} name="password" placeholder="password" className="input input-bordered w-full bg-[#F9FAFB] border border-[#D1D5DB] text-[#333333] focus:ring-2 focus:ring-[#00B4D8] focus:outline-none focus:border-[#00B4D8] placeholder:text-[#A0AEC0]" />
-                            <div onClick={handleEyeButton} className="absolute right-4 top-2 p-2 cursor-pointer">
+                            <input type={isVisible ? 'text' : 'password'} name="password" placeholder="password" className="input input-bordered w-full bg-[#F9FAFB] border border-[#D1D5DB] text-[#333333] focus:ring-2 focus:ring-[#00B4D8] focus:outline-none focus:border-[#00B4D8] placeholder:text-[#A0AEC0] dark:bg-gray-700 dark:text-white" />
+                            <div onClick={handleEyeButton} className="absolute right-4 top-2 p-2 cursor-pointer  dark:text-white">
                                 {
                                     isVisible ?
                                         <span ><FaRegEyeSlash /></span>
@@ -206,7 +206,7 @@ const Register = () => {
                         </div>
 
                         <label className="mt-3">
-                            <span>Already have an account? <Link to={'/login'} className="text-blue-800 font-bold hover:text-[#FFC107]">Login</Link></span>
+                            <span className="dark:text-gray-300">Already have an account? <Link to={'/login'} className="text-blue-800 font-bold hover:text-[#FFC107] dark:text-blue-500 dark:hover:text-[#FFC107]">Login</Link></span>
                         </label>
                     </div>
                     <div>
@@ -215,7 +215,7 @@ const Register = () => {
                         }
                     </div>
                     <div className="form-control mt-6">
-                        <button disabled={registerLoading || loading} className="btn bg-[#003366] text-white hover:bg-[#002B55]">{
+                        <button disabled={registerLoading || loading} className="btn bg-[#003366] text-white hover:bg-[#002B55] dark:bg-primary">{
                             registerLoading ?
                                 <span className="text-xl animate-spin"><BsBrightnessHighFill /></span>
                                 :
@@ -225,7 +225,7 @@ const Register = () => {
                 </form>
                 <div className="divider w-5/6 mx-auto">OR</div>
                 <div className="w-5/6 mx-auto text-center mb-5">
-                    <button onClick={handleGoogleLogin} className="btn w-full bg-[#E0E0E0] hover:bg-[#D1D5DB] text-[#333333]"><span className="text-xl"><FcGoogle /></span> Register With Google</button>
+                    <button onClick={handleGoogleLogin} className="btn w-full bg-[#E0E0E0] hover:bg-[#D1D5DB] text-[#333333] dark:bg-gray-700 dark:text-white "><span className="text-xl"><FcGoogle /></span> Register With Google</button>
                 </div>
             </div>
         </div>
